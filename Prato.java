3d17c2c56d85a2948551fbc4ec157e4bdbe0a937
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-08-15 11:29:13 -0300

    Create Prato.java

---
+++ Prato.java
@@ -0,0 +1,40 @@
+package modelo;
+
+public class Prato {
+	
+	private Integer numero;
+	private String nome;
+	private Double valor;
+	
+	
+		public Prato(Integer numero, String nome, Double valor) {
+		super();
+		this.numero = numero;
+		this.nome = nome;
+		this.valor = valor;
+		
+		
+		return String.format(dados, this.modelo, this.anoFabricacao, fab);
+	}
+		
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
