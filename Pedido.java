3052b0e16a23a043250713ed93abf8357672d183
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-08-15 11:29:54 -0300

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Pedido {
+	
+	private String nomeCliente;
+	private String nomeGarcom;
+	private Prato prato;
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getNomeGarcom() {
+		return nomeGarcom;
+	}
+	public void setNomeGarcom(String nomeGarcom) {
+		this.nomeGarcom = nomeGarcom;
+	}
+	public Prato getPrato() {
+		return prato;
+	}
+	public void setPrato(Prato prato) {
+		this.prato = prato;
+	}
+	
+	
+}
